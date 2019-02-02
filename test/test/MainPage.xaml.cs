530af19ccc78d8403bfa99586550951d18f136a7
Alejandro Agustin Villalba <alejandro_master12@hotmail.com>
2019-02-01 21:19:54 -0300

    Agregue el codigo para abrir los URL en ios. Probar en la mac cloud

--- test/test/MainPage.xaml.cs
+++ test/test/MainPage.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Foundation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UIKit;
 using Xamarin.Forms;
 
 namespace test
@@ -19,23 +21,69 @@ namespace test
         }
         private async void ButtonWpp(object sender, EventArgs args)
         {
-            await Task.Run(() => { Device.OpenUri(new Uri("https://wa.me/5491145281006")); });
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                await Task.Run(() => { Device.OpenUri(new Uri("https://wa.me/5491145281006")); });
+            }
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                await Task.Run(() => { UIApplication.SharedApplication.OpenUrl(new NSUrl("https://wa.me/5491145281006")); });
+            }
         }
         private async void ButtonWeb(object sender, EventArgs args)
         {
-            await Task.Run(() => { Device.OpenUri(new Uri("http://www.copello.com.ar/")); });
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                await Task.Run(() => { Device.OpenUri(new Uri("http://www.copello.com.ar/")); });
+            }
+            if(Device.RuntimePlatform == Device.iOS)
+            {
+                await Task.Run(() => { UIApplication.SharedApplication.OpenUrl(new NSUrl("http://www.copello.com.ar/")); });
+            }
         }
         private async void ButtonExp(object sender, EventArgs args)
         {
-            await Task.Run(() => { Device.OpenUri(new Uri("https://www.pagomiexpensa.com.ar/")); });
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                await Task.Run(() => { Device.OpenUri(new Uri("https://www.pagomiexpensa.com.ar/")); });
+            }
+            if(Device.RuntimePlatform == Device.iOS)
+            {
+                await Task.Run(() => { UIApplication.SharedApplication.OpenUrl(new NSUrl("https://www.pagomiexpensa.com.ar/")); });
+            }
         }
         private async void ButtonCnRed(object sender, EventArgs args)
         {
-            await Task.Run(() => { Device.OpenUri(new Uri("http://www.consorciosenred.com/cerDevelopment/cer_nav.nsf/inicio.html")); });
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                await Task.Run(() => { Device.OpenUri(new Uri("http://www.consorciosenred.com/cerDevelopment/cer_nav.nsf/inicio.html")); });
+            }
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                await Task.Run(() => { UIApplication.SharedApplication.OpenUrl(new NSUrl("http://www.consorciosenred.com/cerDevelopment/cer_nav.nsf/inicio.html")); });
+            }                
         }
         private async void ButtonFb(object sender, EventArgs args)
         {
-            await Task.Run(() => { Device.OpenUri(new Uri("https://m.facebook.com/EstudioRjcopello/")); });
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                await Task.Run(() => { Device.OpenUri(new Uri("https://m.facebook.com/EstudioRjcopello/")); });
+            }
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                await Task.Run(() => { UIApplication.SharedApplication.OpenUrl(new NSUrl("https://m.facebook.com/EstudioRjcopello/")); });
+            }
+        }
+        private async void ButtonIg(object sender, EventArgs e)
+        {
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                await Task.Run(() => { Device.OpenUri(new Uri("https://www.instagram.com/estudiorjcopello/?utm_source=ig_profile_share&igshid=14vrshew50yk2&fbclid=IwAR2TpgzoMXazmMzDNMTF-NfQdpl_Rwi0A1Ifb7h_26QsD4B8bCeuyAUH-bM")); });
+            }
+            if(Device.RuntimePlatform == Device.iOS)
+            {
+                await Task.Run(() => { UIApplication.SharedApplication.OpenUrl(new NSUrl("https://www.instagram.com/estudiorjcopello/?utm_source=ig_profile_share&igshid=14vrshew50yk2&fbclid=IwAR2TpgzoMXazmMzDNMTF-NfQdpl_Rwi0A1Ifb7h_26QsD4B8bCeuyAUH-bM")); });
+            }
         }
         private async void ButtonFaq(object sender, EventArgs args)
         {
@@ -46,10 +94,7 @@ namespace test
             await Navigation.PushAsync(new AcercaDePage());
         }
 
-        private async void ButtonIg(object sender, EventArgs e)
-        {
-            await Task.Run(() => { Device.OpenUri(new Uri("https://www.instagram.com/estudiorjcopello/?utm_source=ig_profile_share&igshid=14vrshew50yk2&fbclid=IwAR2TpgzoMXazmMzDNMTF-NfQdpl_Rwi0A1Ifb7h_26QsD4B8bCeuyAUH-bM")); });
-        }
+       
     }
 
 }
